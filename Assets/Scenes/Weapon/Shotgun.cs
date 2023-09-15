8d69eee47990c745dc6c5cdaa6efecbc2e87f65f
Trung Nguyen <maxherovt123@gmail.com>
2023-09-15 11:13:26 -0400

    Rate of Fire
    Adjust the variable to control the rate of fire.

--- Assets/Scenes/Weapon/Shotgun.cs
+++ Assets/Scenes/Weapon/Shotgun.cs
@@ -28,6 +28,9 @@ public class Shotgun : MonoBehaviour
     [SerializeField] float shakeStrength;
     [SerializeField] float impactForce;
     public bool isShooting = false;
+
+    private float lastShotTime;
+
     public enum InputMethod
     {
         None,
@@ -69,44 +72,56 @@ public class Shotgun : MonoBehaviour
     {
         if (readyToShoot && !reloading && bulletsLeft > 0)
         {
-            bulletsShot = bulletsPerTap;
-            readyToShoot = false;
-            Debug.Log(inputMethod);
 
-            if (inputMethod == InputMethod.LeftClick ||
-            (inputMethod == InputMethod.RightClick))
+            // Calculate the time since the last shot
+            float timeSinceLastShot = Time.time - lastShotTime;
+
+            if (timeSinceLastShot >= timeBetweenShooting)
             {
-                float x = Random.Range(-spread, spread);
-                float y = Random.Range(-spread, spread);
-                Vector3 direction = fpsCam.transform.forward + new Vector3(x, y, 0);
 
-                if (Physics.Raycast(fpsCam.transform.position, direction, out rayHit, range, whatIsEnemy))
+                bulletsShot = bulletsPerTap;
+                readyToShoot = false;
+                Debug.Log(inputMethod);
+
+                // Update the last shot time
+                lastShotTime = Time.time;
+
+                if (inputMethod == InputMethod.LeftClick ||
+           (inputMethod == InputMethod.RightClick))
                 {
-                    Debug.Log(rayHit.collider.name);
+                    float x = Random.Range(-spread, spread);
+                    float y = Random.Range(-spread, spread);
+                    Vector3 direction = fpsCam.transform.forward + new Vector3(x, y, 0);
 
-                    /* if (rayHit.collider.CompareTag("Ground") || rayHit.collider.CompareTag("Wall"))
-                     {
-                         Quaternion impactRotation = Quaternion.LookRotation(rayHit.normal);
-                         // GameObject impact = Instantiate(bulletHoleGraphic, rayHit.point, impactRotation);
-                         // impact.transform.parent = rayHit.transform;
-                     }*/
-
-                    if (rayHit.rigidbody != null)
+                    if (Physics.Raycast(fpsCam.transform.position, direction, out rayHit, range, whatIsEnemy))
                     {
-                        rayHit.rigidbody.AddForce(-rayHit.normal * impactForce);
-                    }
-                }
-                bulletsLeft--;
-                bulletsShot--;
+                        Debug.Log(rayHit.collider.name);
 
-                Invoke("ResetShot", timeBetweenShooting);
-                if (isRecoil)
-                {
-                    playerRB.AddForce(-direction.normalized * recoilForce, ForceMode.VelocityChange);
+                        /* if (rayHit.collider.CompareTag("Ground") || rayHit.collider.CompareTag("Wall"))
+                         {
+                             Quaternion impactRotation = Quaternion.LookRotation(rayHit.normal);
+                             // GameObject impact = Instantiate(bulletHoleGraphic, rayHit.point, impactRotation);
+                             // impact.transform.parent = rayHit.transform;
+                         }*/
+
+                        if (rayHit.rigidbody != null)
+                        {
+                            rayHit.rigidbody.AddForce(-rayHit.normal * impactForce);
+                        }
+                    }
+                    bulletsLeft--;
+                    bulletsShot--;
+
+                    Invoke("ResetShot", timeBetweenShooting);
+                    if (isRecoil)
+                    {
+                        playerRB.AddForce(-direction.normalized * recoilForce, ForceMode.VelocityChange);
+                    }
+                    if (bulletsShot > 0 && bulletsLeft > 0)
+                        Invoke("Shoot", timeBetweenShots);
                 }
-                if (bulletsShot > 0 && bulletsLeft > 0)
-                    Invoke("Shoot", timeBetweenShots);
             }
+               
         }      
         
     }
