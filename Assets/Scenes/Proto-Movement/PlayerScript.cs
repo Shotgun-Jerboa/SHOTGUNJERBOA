2d0844ea68cce24334e04c9f665163894740d435
Trung Nguyen <maxherovt123@gmail.com>
2023-09-18 19:22:24 -0400

    Week 2 Task
    -The recoil should be at a fixed distance whenever you shoot (falling down, on the grounded, etc.).
    -Create another scene for shotgun testing to avoid merge conflict.

--- Assets/Scenes/Proto-Movement/PlayerScript.cs
+++ Assets/Scenes/Proto-Movement/PlayerScript.cs
@@ -7,6 +7,7 @@ public class PlayerScript : MonoBehaviour
     public float gravityConstant;
 
     private Vector3 velocity;
+    public bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +19,19 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
 
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
