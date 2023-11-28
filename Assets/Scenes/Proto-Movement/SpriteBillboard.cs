23c28e41383dbd591d5217b7619f3232fd601104
KCWoodring <146131293+KCWoodring@users.noreply.github.com>
2023-11-28 17:49:12 -0500

    Updated Billboard
    I got rid of the freeze axis because it is not really needed given the player will be jumping

--- Assets/Scenes/Proto-Movement/SpriteBillboard.cs
+++ Assets/Scenes/Proto-Movement/SpriteBillboard.cs
@@ -2,19 +2,11 @@ using UnityEngine;
 
 public class SpriteBillboard : MonoBehaviour
 {
-    
-    [SerializeField] bool freezeXZAxis = true;
+  
     // Update is called once per frame
     void Update()
     {
-        if (freezeXZAxis)
-        {
-            transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
-        }
         
-        else
-        {
-            transform.rotation = Camera.main.transform.rotation;
-        }
+        transform.rotation = Camera.main.transform.rotation;
     }
 }
