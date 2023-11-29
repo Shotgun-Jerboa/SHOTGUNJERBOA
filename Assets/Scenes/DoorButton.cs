345df09a2bbc56aa0ee728b86ad00fbb5365b4bf
Trung Nguyen <maxherovt123@gmail.com>
2023-11-29 15:29:03 -0500

    Adding the set up the for eating

--- Assets/Scenes/DoorButton.cs
+++ Assets/Scenes/DoorButton.cs
@@ -4,6 +4,31 @@ using UnityEngine;
 
 public class DoorButton : MonoBehaviour
 {
-    
+    public bool isPressed = false;
+    public float moveSpeed = 5f;
+    public Vector3 targetPosition;
 
+    private Vector3 startPosition;
+    private float startTime;
+    private bool isMoving = false;
+    Animator doorAnimator;
+    Animator buttonAnimator;
+
+
+
+    private void Start()
+    {
+        buttonAnimator = GetComponent<Animator>();
+                startPosition = transform.position;
+
+    }
+    private void Update()
+    {
+        if (isPressed)
+        {
+            buttonAnimator.SetBool("IsPressed", true);
+        }
+
+
+    }
 }
